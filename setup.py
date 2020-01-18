534600f1ca9bff3bb303dd01dc3d3a4d5d4c98ec
Av3m <7688354+Av3m@users.noreply.github.com>
2020-01-18 20:20:04 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ConanToolbox',
+    version='1.0',
+    packages=['conantool',],
+    license='MIT',
+    long_description="additional Toolbox for conan.io C/C++ package manager",
+)
