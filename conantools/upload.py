6c372ed320878e7be810f2ce1ad5c28b2d7579b1
Av3m <7688354+Av3m@users.noreply.github.com>
2020-01-18 21:45:38 +0100

    extend Readme

--- conantools/upload.py
+++ conantools/upload.py
@@ -7,26 +7,81 @@ import json
 from .common.utils import *
 from .common.globals import *
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='upload a package and ALL its dependencies to a remote server')
-    parser.add_argument('reference', metavar='reference', type=str,help='conanfile dir or reference')
-    parser.add_argument('--profile','-pr',type=str, metavar='profile', help="Apply the specified profile to the install command")
-    parser.add_argument('--remote','-r',type=str, metavar='remote', help="conan remote for upload",  required=True)
-    parser.add_argument('--options','-o',type=str, metavar='PROFILE', nargs="+", help="cDefine options values, e.g., -o Pkg:with_qt=True")
-    parser.add_argument('--env','-e',type=str, metavar='e', nargs="+", help="co for upload")
-    parser.add_argument('--settings','-s',type=str, metavar='SETTINGS', nargs="+", help="Settings to build the package, overwriting the defaults. e.g., -s compiler=gcc")
-    parser.add_argument('--check',action='store_true', help="perform an integrity check, using the manifests,before upload ")
-    parser.add_argument('--all', action='store_true', help="Upload both package recipe and packages")
-    parser.add_argument('--skip-upload', action='store_true', help="Upload both package recipe and packages")
-    parser.add_argument('--confirm','-c', action='store_true', help="Upload all matching recipes without confirmation")
-    parser.add_argument('--dry-run', action='store_true', help="Do not upload, only show what would be done")
-    parser.add_argument('--parallel',action='store_true', help="Upload files in parallel using multiple threads the default number of launched threads is 8")
-    parser.add_argument('--retry',type=int, metavar="RETRY", help="In case of fail retries to upload again the specified times.")
-    parser.add_argument('--retry-wait',type=int, metavar="SEC", help="Waits specified seconds before retry again")
-    parser.add_argument('-no', '--no-overwrite',type=str, metavar="{all, recipe}", help="Uploads package only if recipe is the same as the remote one")
+    parser.add_argument('reference',
+                        metavar='reference',
+                        type=str,
+                        help='conanfile dir or reference')
 
-    args = parser.parse_args()
-    return args
+    parser.add_argument('--profile', '-pr',
+                        type=str,
+                        metavar='profile',
+                        help="Apply the specified profile to the install command")
+    parser.add_argument('--remote', '-r',
+                        type=str,
+                        metavar='remote',
+                        help="conan remote for upload",
+                        required=True)
+
+    parser.add_argument('--options', '-o',
+                        type=str,
+                        metavar='PROFILE',
+                        nargs="+",
+                        help="cDefine options values, e.g., -o Pkg:with_qt=True")
+    parser.add_argument('--env', '-e',
+                        type=str,
+                        metavar='e',
+                        nargs="+",
+                        help="co for upload")
+
+    parser.add_argument('--settings', '-s',
+                        type=str,
+                        metavar='SETTINGS',
+                        nargs="+",
+                        help="Settings to build the package, overwriting the defaults. e.g., -s compiler=gcc")
+
+    parser.add_argument('--check',
+                        action='store_true',
+                        help="perform an integrity check, using the manifests,before upload ")
+
+    parser.add_argument('--all',
+                        action='store_true',
+                        help="Upload both package recipe and packages")
+
+    parser.add_argument('--skip-upload',
+                        action='store_true',
+                        help="Upload both package recipe and packages")
+
+    parser.add_argument('--confirm', '-c',
+                        action='store_true',
+                        help="Upload all matching recipes without confirmation")
+
+    parser.add_argument('--dry-run',
+                        action='store_true',
+                        help="Do not upload, only show what would be done")
+    parser.add_argument('--parallel',
+                        action='store_true',
+                        help="Upload files in parallel using multiple "
+                             "threads the default number of launched threads is 8")
+
+    parser.add_argument('--retry',
+                        type=int,
+                        metavar="RETRY",
+                        help="In case of fail retries to upload again the specified times.")
+
+    parser.add_argument('--retry-wait',
+                        type=int,
+                        metavar="SEC",
+                        help="Waits specified seconds before retry again")
+
+    parser.add_argument('-no', '--no-overwrite',
+                        type=str,
+                        metavar="{all, recipe}",
+                        help="Uploads package only if recipe is the same as the remote one")
+
+    return parser.parse_args()
 
 
 def get_packages(json_data):
@@ -86,7 +141,7 @@ def get_packages(json_data):
 
 
 def upload_packages(packages, args):
-    upload_cmd = ['conan', 'upload']
+    upload_cmd = [conan_exe, 'upload']
 
     if args.all:
         upload_cmd += ['--all']
@@ -127,7 +182,7 @@ if __name__ == "__main__":
 
     json_file = tempfile.mkstemp()[1]
 
-    info_command = ['conan', 'info', args.reference, '-j', json_file]
+    info_command = [conan_exe, 'info', args.reference, '-j', json_file]
     if args.profile:
         info_command += ['-pr', args.profile]
 
